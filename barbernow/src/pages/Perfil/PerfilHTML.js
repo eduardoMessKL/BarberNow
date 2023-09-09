4ce0eaa6789c7b6976bc4235cccad162a1ea4a99
Gabriel Caldas <gabriel.caldas20112002@gmail.com>
2023-09-09 14:34:32 -0300

    commit

---
+++ barbernow/src/pages/Perfil/PerfilHTML.js
@@ -0,0 +1,19 @@
+import React from "react";
+import arrowImg from "../../assets/arrow.svg";
+import logoImg from "../../assets/logo.svg";
+
+function PerfilHTML(props) {
+  return (
+<div className="container">
+    <header className="header">
+      <img src={logoImg} alt="Workflow" className="logoImg" />
+      <span>Bem-vindo ao barberNow</span>
+    </header>
+    <a href="/cadastro"><button>Cadastro</button></a>
+    <a href='/login'><button>Login</button></a>
+  </div>
+  );
+
+}
+
+export default PerfilHTML;
