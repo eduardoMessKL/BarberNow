bc1248e9813f0dbc0b1de13756bea1c3a81a11ae
Gabriel Caldas <gabriel.caldas20112002@gmail.com>
2023-09-09 14:38:57 -0300

    commit

--- barbernow/src/routes/AppRoutes.js
+++ barbernow/src/routes/AppRoutes.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import {Home} from "../pages/Home/Home"
 import { Login } from "../pages/Login/Login"
 import { Cadastro} from "../pages/Cadastro/Cadastro"
 import {Perfil} from "../pages/Perfil/perfil"
