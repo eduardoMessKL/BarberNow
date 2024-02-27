bfe766b934195830508621e176a807afbee22804
IsraelHF <israel.com.ihf80@gmail.com>
2024-02-27 19:54:41 -0300

    Removido loading e importações não utilizadas

--- barbernow/src/pages/Login/Login.js
+++ barbernow/src/pages/Login/Login.js
@@ -4,22 +4,23 @@ import { Link, useNavigate } from "react-router-dom";
 import { db, auth } from "../../firebase/firebaseConfig";
 import LoginHTML from "./LoginHTML";
 import "./Login.css";
-import { getBarbearia } from "../../model/services/BarbeariaService";
 import { query, where, getDocs, collection } from "firebase/firestore";
 
 export function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+  const [signInWithEmailAndPassword, user, error] =
+    useSignInWithEmailAndPassword(auth);
 
   async function handleSignIn(e) {
     e.preventDefault();
-  
+
     try {
       const response = await signInWithEmailAndPassword(email, password);
 
-      if (response.user) {  // Verificar se houve um retorno válido de usuário
+      if (response.user) {
+        // Verificar se houve um retorno válido de usuário
         const q = query(
           collection(db, "barbearias"),
           where("email", "==", email)
@@ -32,19 +33,16 @@ export function Login() {
           const cnpj = userData.cnpj;
           navigate(`/perfil/${cnpj}`);
         } else {
-          alert("Erro ao fazer login: usuário não encontrado")
-          console.error("No user found with the given email in barbearias collection");
+          alert("Erro ao fazer login: usuário não encontrado");
+          console.error(
+            "No user found with the given email in barbearias collection"
+          );
         }
       }
-
     } catch (signInError) {
       console.error("Error during sign-in:", signInError);
       alert("Erro ao fazer login: " + signInError.message);
     }
-  }
-
-  if (loading) {
-    return <p>carregando...</p>;
   }
 
   return (
