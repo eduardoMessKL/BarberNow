6cb4711ed0683e7932385c15a42a78a981467580
IsraelHF <israel.com.ihf80@gmail.com>
2024-02-27 21:23:31 -0300

    Edição de serviços funcionando, e adicionado botão de cancelar edição

--- barbernow/src/pages/Editar/Serviço/ServicosE.js
+++ barbernow/src/pages/Editar/Serviço/ServicosE.js
@@ -1,67 +1,75 @@
 import React, { useState, useEffect } from "react";
-import { getServicosByBarbearia, updateServico, getServico } from "../../../model/services/ServicoService";
+import {
+  getServicosByBarbearia,
+  updateServico,
+  getServico,
+} from "../../../model/services/ServicoService";
 import { getBarbearia } from "../../../model/services/BarbeariaService";
 import { useParams } from "react-router-dom";
-import EditarServicosHTML from "./ServicosHTMLE";  // Ajuste para importar o componente correto
+import EditarServicosHTML from "./ServicosHTMLE"; // Ajuste para importar o componente correto
 import "./ServicosE.css";
 
 export function ServicosE() {
-  const [servico, setServico] = useState(null); 
+  const [servico, setServico] = useState(null);
   const [barbearia, setBarbearia] = useState({});
   const [formValues, setFormValues] = useState({});
   const { cnpj, servicoID } = useParams();
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const servicoData = await getServico(cnpj, servicoID);
-                const barbeariaData = await getBarbearia(cnpj);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const servicoData = await getServico(cnpj, servicoID);
+        const barbeariaData = await getBarbearia(cnpj);
 
-                setServico(servicoData);
-                setBarbearia(barbeariaData);
+        setServico(servicoData);
+        setBarbearia(barbeariaData);
 
-                // Inicializa o estado 'formValues' com os valores dos serviços
-                const initialFormValues = {};
-                servicoData.forEach(servico => {
-                    initialFormValues[servico.id] = {
-                        nome: servico.nome,
-                        descricao: servico.descricao,
-                        duracao: servico.duracao,
-                        fotoURL: servico.fotoURL,
-                        preco: servico.preco,
-                        tipo: servico.tipo
-                    };
-                });
-                setFormValues(initialFormValues);
-            } catch (error) {
-                console.error("Erro ao buscar dados: ", error);
-            }
-        }
+        // Inicializa o estado 'formValues' com os valores do serviço
+        setFormValues({
+          nome: servicoData.nome,
+          descricao: servicoData.descricao,
+          duracao: servicoData.duracao,
+          fotoURL: servicoData.fotoURL,
+          preco: servicoData.preco,
+          tipo: servicoData.tipo,
+        });
+      } catch (error) {
+        console.error("Erro ao buscar dados: ", error);
+      }
+    }
 
-        fetchData();
-    }, [cnpj,servicoID]);
+    fetchData();
+  }, [cnpj, servicoID]);
 
-    const handleUpdate = async (servicoID) => {
-        try {
-            // Use formValues[servicoID] para obter os valores atualizados deste serviço
-            await updateServico(cnpj, servicoID, formValues[servicoID]);
-            console.log("Serviço atualizado com sucesso!");
+  const handleChange = (field, value) => {
+    setFormValues((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  };
 
-            // Recarrega os serviços após a atualização para refletir as mudanças no UI
-            const updatedServicos = await getServicosByBarbearia(cnpj);
-            setServico(updatedServicos);
-        } catch (error) {
-            console.error("Erro ao atualizar o serviço: ", error);
-        }
-    };
+  const handleUpdate = async () => {
+    try {
+      // Use formValues para obter os valores atualizados do serviço
+      await updateServico(cnpj, servicoID, formValues);
+      console.log("Serviço atualizado com sucesso!");
 
-    return (
-        <EditarServicosHTML 
-            barbearia={barbearia}
-            servico={servico} 
-            formValues={formValues} 
-            setFormValues={setFormValues} 
-            handleUpdate={handleUpdate} 
-        />
-    );
+      // Recarrega os serviços após a atualização para refletir as mudanças no UI
+      const updatedServicos = await getServicosByBarbearia(cnpj);
+      setServico(updatedServicos);
+    } catch (error) {
+      console.error("Erro ao atualizar o serviço: ", error);
+    }
+  };
+
+  return (
+    <EditarServicosHTML
+      barbearia={barbearia}
+      servico={servico}
+      formValues={formValues}
+      setFormValues={setFormValues}
+      handleUpdate={handleUpdate}
+      handleChange={handleChange}
+    />
+  );
 }
